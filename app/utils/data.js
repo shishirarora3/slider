f129a9671306dcfb16545213fb776a7edaa9d2ba
Shishir <shishir.arora@sprinklr.com>
2016-03-17 16:49:28 +0530

    initial

--- app/utils/data.js
+++ app/utils/data.js
@@ -1,5 +1,4 @@
 var myList = document.querySelector('ul');
-var myRequest = new Request('products.json');
 fetch(myRequest).then(function(response) {
     return response.json().then(function(json) {
         for(i = 0; i < json.products.length; i++) {
@@ -16,7 +15,7 @@ var imageFactory = {
     counter:0,
     request,
     next: function(noOfSlides,source){
-        imageFactory.request = imageFactory.request || new Request('https://api.myjson.com/bins/1wqfa')
+        imageFactory.request = imageFactory.request || new Request(source);
         fetch(imageFactory.request).then(function(response) {
             return response.json();
         }).then((json)=>{
